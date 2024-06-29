e4636f68d1d30f2dc6909b8e65dcaccf8daa60f0
ToneyG <wamunyu57@gmail.com>
2024-06-30 00:58:46 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a');
+    
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
